872a71d4d461bbf5ec45aeb64c29b9c9759fa0d6
falgan <fatih.algan@gmail.com>
2016-04-20 20:48:28 +0300

    Update

--- test/com/bridge/pelatro/test/RetrieveOfferBouquet.java
+++ test/com/bridge/pelatro/test/RetrieveOfferBouquet.java
@@ -1,5 +1,34 @@
 package com.bridge.pelatro.test;
 
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+
+import com.bridge.pelatro.command.CommandFactory;
+import com.bridge.pelatro.command.RetrieveOptInOffersCommand;
+
 public class RetrieveOfferBouquet {
 
+	private CommandFactory commandFactory;
+	private String msisdn = "258827788383";
+	
+	private static Logger logger = Logger.getLogger(RetrieveOfferBouquet.class);
+	
+	@Before
+	public void setUp() throws Exception {
+		commandFactory = new CommandFactory("http://10.100.58.15:8282", "10.100.58.15");
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	public void testRetrieveOptionalOffers() throws Exception {
+		try {
+			RetrieveOptInOffersCommand cmd = commandFactory.getRetrieveOptInOffersCommand(msisdn);
+			cmd.execute();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
